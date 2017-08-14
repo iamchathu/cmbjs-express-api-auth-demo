a0608a20dc0307d6014af9d86412e0fb87b3bbc7
Chathu Vishwajith <4evercreations@gmail.com>
2017-08-14 17:26:35 +0530

    Add clear collection before seed User

--- modules/core/seed.controller.js
+++ modules/core/seed.controller.js
@@ -8,11 +8,11 @@ const User = mongoose.model('User');
  */
 module.exports.seedUser = (req, res) => {
   if (config.seedDB.seed) {
+    User.remove({});
     const user = new User({email: 'iam@chathu.me', firstName: 'Chathu', lastName: 'Vishwajith', password: '1234@abc'});
     user.save();
     res.status(200).send('Database seeded!');
   } else {
     res.status(401).send('Seed not allowed !');
   }
-
 }
